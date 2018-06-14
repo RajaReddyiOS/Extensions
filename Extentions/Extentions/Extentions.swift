ee19893f910678ea8595c896c0e9ab53ad2c8ca2
raja_reddy <raja@cogcons.com>
2018-06-14 15:34:08 +0530

    finish changes

---
+++ Extentions/Extentions/Extentions.swift
@@ -0,0 +1,19 @@
+//
+//  Extentions.swift
+//  Extentions
+//
+//  Created by Raja on 14/06/18.
+//  Copyright © 2018 Cogcons. All rights reserved.
+//
+
+import Foundation
+
+open class Extensions {
+    
+    fileprivate init() {}
+    
+    open static func doSomething() -> String {
+        return "you did something"
+    }
+    
+}
